599972673d4288dfaf5f8a280e5dd7a1a2836504
g.saintetienne <g.saintetienne@primobox.com>
2021-11-24 22:17:14 +0100

    plus de code, plus de tests

--- PrimeService.Tests/UnitTest1.cs
+++ PrimeService.Tests/UnitTest1.cs
@@ -6,11 +6,152 @@ namespace PrimeService.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void TestSum()
+          [Fact]
+        public void ConcatValues_ShouldReturn_CorrectValue()
         {
-            var actual = Utilities.Sum(1, 0);
-            Assert.Equal(1, actual);
+            var result = Example.ConcatValues("foo", "bar");
+            
+            Assert.Equal("foobar", result);
+        }
+
+        [Fact]
+        public void CountWordsInText_ShouldReturn_CorrectValue()
+        {
+            var result = Example.CountWordsInText("foo bar");
+            
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void RemoveCharsAndSymbols_ShouldReturn_CorrectValue()
+        {
+            var result = Example.RemoveCharsAndSymbols("abcdef_!@*&*@(*%1234");
+            
+            Assert.Equal("1234", result);
+        }
+
+        [Fact]
+        public void SumTwoValues_ShouldReturn_CorrectValue()
+        {
+            var result = Example.SumTwoValues(1, 1);
+            
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void MultiplyTwoValues_ShouldReturn_CorrectValue()
+        {
+            var result = Example.MultiplyTwoValues(2, 2);
+            
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void FilterValuesBiggerThan_ShouldReturn_CorrectValues()
+        {
+            var result = Example.FilterValuesBiggerThan(50, 1, 50, 100, 200, 300, 400);
+            
+            Assert.Collection(result,
+                first => Assert.Equal(100, first),
+                second => Assert.Equal(200, second),
+                third => Assert.Equal(300, third),
+                fourth => Assert.Equal(400, fourth));
+        }
+
+        [Fact]
+        public void VerifyNumberIsPrime_ShouldReturn_True()
+        {
+            var result = Example.VerifyNumberIsPrime(3);
+            
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(4)]
+        public void VerifyNumberIsPrime_ShouldReturn_False(uint number)
+        {
+            var result = Example.VerifyNumberIsPrime(number);
+            
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void VerifyNumberIsComposite_ShouldReturn_True()
+        {
+            var result = Example.VerifyNumberIsComposite(9);
+            
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(5)]
+        public void VerifyNumberIsComposite_ShouldReturn_False(uint number)
+        {
+            var result = Example.VerifyNumberIsComposite(number);
+            
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(100)]
+        public void VerifyNumberIsNatural_ShouldReturn_True(int number)
+        {
+            var result = Example.VerifyNumberIsNatural(number);
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void VerifyNumberIsNatural_ShouldReturn_False()
+        {
+            var result = Example.VerifyNumberIsNatural(-100);
+            
+            Assert.False(result);
+        }
+        
+        [Theory]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void CalculateFibonacci_WithMaxValue_LastValue_ShouldBe_Eigth(int maxValue)
+        {
+            var result = Example.CalculateFibonacci(maxValue);
+            
+            Assert.Equal(8, result[^1]);
+        }
+        
+        [Theory]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void CalculateFibonacci_WithMaxValue_ShouldReturn_CorrectValues(int maxValue)
+        {
+            var result = Example.CalculateFibonacci(maxValue);
+            
+            Assert.Equal(new[] { 0, 1, 1, 2, 3, 5, 8 }, result);
+        }
+        
+        [Fact]
+        public void CalculateFibonacci_WithMaxOne_ShouldReturn_CorrectValues()
+        {
+            var result = Example.CalculateFibonacci(1);
+            
+            Assert.Equal(new[] { 0, 1, 1 }, result);
+        }
+        
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void CalculateFibonacci_WithMaxValue_ShouldReturn_Default(int maxValue)
+        {
+            var result = Example.CalculateFibonacci(maxValue);
+            
+            Assert.Equal(new[] { 0, 1, 1 }, result);
         }
     }
 }
--- PrimeService/Utilities.cs
+++ PrimeService/Utilities.cs
@@ -1,12 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace PrimeService
 {
-    public static class Utilities
+    public static class Example
     {
-        public static int Sum(int a, int b)
+         public static string ConcatValues(params string[] values)
         {
-            return a + b;
+            return string.Join(string.Empty, values);
+        }
+
+        public static int CountWordsInText(string text)
+        {
+            return string.IsNullOrWhiteSpace(text) ? default : text.Split(" ").Length;
+        }
+
+        public static string RemoveCharsAndSymbols(string value)
+        {
+            return Regex.Match(value, @"\d+").Value;
+        }
+
+        public static double SumTwoValues(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+
+        public static double MultiplyTwoValues(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+
+        public static IEnumerable<double> FilterValuesBiggerThan(double valueToCompare, params double[] valuesToFilter)
+        {
+            return valuesToFilter
+                .Where(val => val > valueToCompare);
+        }
+
+        public static bool VerifyNumberIsPrime(uint number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (var i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        
+        public static bool VerifyNumberIsComposite(uint number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (var i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return true;
+            }
+
+            return false;
+        }
+        
+        public static bool VerifyNumberIsNatural(int number)
+        {
+            return number >= 0;
+        }
+        
+        public static int[] CalculateFibonacci(int maxValue = 100, int[] sequence = default)
+        {
+            if ((sequence ??= new[] { 0, 1, 1 })[^1] == maxValue)
+                return sequence;
+
+            var newValue = sequence[^1] + sequence[^2];
+            return newValue > maxValue ? sequence : CalculateFibonacci(maxValue, sequence.Concat(new[] { newValue }).ToArray());
         }
     }
 }
